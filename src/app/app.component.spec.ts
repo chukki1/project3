3cd6978a687e67c999df1d2b140acc8293308a12
Chukki <ayeshaekanayaka1995@gmail.com>
2018-06-09 11:41:33 +0530

    change app title to FIT

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,22 +6,22 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents();ng test
   }));
-  it('should create the app', async(() => {
+  it('should create the FIT', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'FIT'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('FIT');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FIT!');
   }));
 });
